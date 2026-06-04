12d35bcdad67b782d921fb2dae8de7932d2bc042
LuisinaSierra <52178013@ivt.edu.ar>
2026-06-04 10:30:21 -0300

    TP5 4
    A

--- TP5/app.js
+++ TP5/app.js
@@ -1,12 +1,6 @@
 
-let n1 = 4;
-let n2 = 2;
+let nombreAlumno = 'Arthur '
+let apellidoAlumno = 'Kirkland '
+let edadAlumno = '634 '
 
-let suma = n1 + n2;
-let resta = n1 - n2;
-let mult = n1 * n2;
-let div = n1 / n2;
-
-
-alert('el resultado de la suma es: ' + suma);
-console.log('el resultado de la resta es: ' + resta);
+console.log('El alumno: ' + nombreAlumno + apellidoAlumno + 'tiene ' + edadAlumno + 'años')
