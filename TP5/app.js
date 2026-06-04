8dc7b82c83bd235636df206525e3044a95b2dd84
LuisinaSierra <52178013@ivt.edu.ar>
2026-06-04 10:33:51 -0300

    TP5 5
    A

--- TP5/app.js
+++ TP5/app.js
@@ -1,6 +1,7 @@
 
-let nombreAlumno = 'Arthur '
-let apellidoAlumno = 'Kirkland '
-let edadAlumno = '634 '
+let price1 = 11
+let price2 = 21
+let price3 = 31
+let total = price1 + price2 + price3
 
-console.log('El alumno: ' + nombreAlumno + apellidoAlumno + 'tiene ' + edadAlumno + 'años')
+console.log('El total a pagar es ' + total)
