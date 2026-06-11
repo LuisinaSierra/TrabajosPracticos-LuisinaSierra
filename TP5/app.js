2843aca92ee8fa36594ec1caf93d3922a7ee22fa
alumno <52178013@ivt.edu.ar>
2026-06-11 10:27:27 -0300

    TP5 (completo)

--- TP5/app.js
+++ TP5/app.js
@@ -1,4 +1,55 @@
+let numero1 = 10
+let numero2 = 2
+
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let multiplicacion = numero1 * numero2
+let Division = numero1 / numero2
+
+
+alert(suma);
+alert(resta);
+
+console.log(multiplicacion);
+console.log(Division);
+
+
+
+alert('el resultado de la suma es: ' + suma);
+console.log('el resultado de la resta es: ' + resta);
+
+
+
+
+let nombreAlumno = 'Arthur'
+let apellidoAlumno = 'Kirkland'
+let edadAlumno = 23
+
+console.log('El alumno: ' + nombreAlumno + ' ' + apellidoAlumno + ' tiene ' + edadAlumno + ' años');
+
+
+
+
+let precio1 = 6700
+let precio2 = 6767
+let precio3 = 6007
+let Total = precio1 + precio2 + precio3
+
+console.log('El total a pagar es: ' + '$' + Total);
+
+
+
+
+let comidafavorita = 'fanta de uva'
+let colorfavorito = 'violeta'
+let numerofavorito = 2
+
+console.log('Mi comida favorita es ' + comidafavorita + ', me gusta el color ' + colorfavorito + ' y mi número favorito es el ' + numerofavorito);
+
+
+
+
 let pais = 'Argentina'
-console.log(pais)
+console.log(pais);
 pais = 'Brasil'
-console.log(pais)
+console.log(pais);
