19f88de910f9adf5723fd4a33b0b764110daef22
alumno <52178013@ivt.edu.ar>
2026-06-11 10:05:24 -0300

    TP5 7

--- TP5/app.js
+++ TP5/app.js
@@ -1,6 +1,4 @@
-
-let comidafav = 'fanta de uva'
-let colorfav = 'violeta'
-let numfav = 2
-
-console.log('Mi comida favorita es ' + comidafav + ', me gusta el color ' + colorfav + ' y mi numero fav es el' + numfav)
+let pais = 'Argentina'
+console.log(pais)
+pais = 'Brasil'
+console.log(pais)
