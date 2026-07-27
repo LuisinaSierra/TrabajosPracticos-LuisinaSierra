213fa7adbf5026e2bf987396a6f88adf3dc578e8
LuisinaSierra <52178013@ivt.edu.ar>
2026-07-27 08:18:31 -0300

    tp6 2

--- TP6/app.js
+++ TP6/app.js
@@ -2,8 +2,16 @@ let btn1 = document.querySelector('#btn1')
 let btn2 = document.querySelector('#btn2')
 let btn3 = document.querySelector('#btn3')
 let btn4 = document.querySelector('#btn4')
-let text = document.querySelector('p')
+let text = document.querySelector('#text')
 
-btn1.onclick = function() {
-text.style.backgroundColor = 'red';
+btn1.onclick = function () {
+    text.style.backgroundColor = 'red'
+}
+
+btn2.onclick = function () {
+    text.textContent = 'hola el texto cambio hola'
+}
+
+btn3.onclick = function() {
+    text.style.color = 'white'
 }
