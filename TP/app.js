add696fd5c7b124853d53a05d73babf85eb31961
LuisinaSierra <52178013@ivt.edu.ar>
2026-09-17 10:39:12 -0300

    TP COOKIE2

--- TP/app.js
+++ TP/app.js
@@ -3,6 +3,28 @@ let text = document.querySelector('#text')
 let boton = document.querySelector('#boton')
 
 boton.onclick = function() {
-    numero = numero + 1
-    text.textContent = numero
+    numero = numero + 5
+    text.textContent = numero + ' ' + 'carrot'
+
+    if (numero >= 10) {
+        text.style.color = 'red'
+    }
+    if (numero >= 20) {
+        text.style.color = 'orange'
+    }
+    if (numero >= 30){
+        text.style.color = 'yellow'
+    }
+    if (numero >= 40) {
+        text.style.color = 'green'
+    }
+    if (numero >= 50) {
+        text.style.color = 'blue'
+    }
+    if (numero >= 60) {
+        text.style.color = 'purple'
+    }
+    if (numero >= 70) {
+        text.style.color = 'pink'
+    }
 }
