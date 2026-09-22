ac89cf184d7cc38f74838281ff09239ef2052917
LuisinaSierra <52178013@ivt.edu.ar>
2026-09-22 09:46:27 -0300

    TP CLICKER

--- TP/app.js
+++ TP/app.js
@@ -1,30 +1,28 @@
 let numero = 0
 let text = document.querySelector('#text')
 let boton = document.querySelector('#boton')
+let mult = document.querySelector('#x2')
+let suma = 1
 
 boton.onclick = function() {
-    numero = numero + 5
+    numero = numero + suma
     text.textContent = numero + ' ' + 'carrot'
 
     if (numero >= 10) {
-        text.style.color = 'red'
-    }
-    if (numero >= 20) {
-        text.style.color = 'orange'
-    }
-    if (numero >= 30){
-        text.style.color = 'yellow'
-    }
-    if (numero >= 40) {
         text.style.color = 'green'
     }
-    if (numero >= 50) {
+    if (numero >= 20) {
         text.style.color = 'blue'
     }
-    if (numero >= 60) {
-        text.style.color = 'purple'
+    if (numero >= 30) {
+        text.style.color = 'red'
     }
-    if (numero >= 70) {
-        text.style.color = 'pink'
+}
+
+mult.onclick = function() {
+    if (numero >= 20) {
+        numero = numero - 20
+        text.textContent = numero + ' ' + 'carrot'
+        suma = suma * 2
     }
 }
